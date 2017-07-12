019b52fab3edc0c09404903108e7308c8b5bcc73
maxfield-chen <maxfieldchen@gmail.com>
2017-07-11 17:03:51 -0700

    Clean up and make generic (ecb_oracle_obaat)

--- set2/obaat_ecb_decrypt.py
+++ set2/obaat_ecb_decrypt.py
@@ -16,7 +16,7 @@ def cm_oracle(offer):
             seen[cb] = True
     return True
 
-def ecb_oracle_attack():
+def ecb_oracle_attack(mystery):
     #Detect Block Size
     key = mc.srand(16)
     ioffer = offering("B", key)
@@ -38,16 +38,16 @@ def ecb_oracle_attack():
         print "ECB MODE"
 
     # Offend the Oracle
-    mystery = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK".decode("base-64")
     cur_mystery = ""
     answer = ""
 
-    for block in xrange(len(mystery)/block_size + 1):
+    num_blocks = len(mystery)/block_size
+    if (num_blocks * block_size != len(mystery)):
+        num_blocks += 1
+    for block in xrange(num_blocks):
         block = block * block_size
-        print block,":",block+block_size
         cur_mystery = mystery[block:block+block_size]
         cur_answer = ""
-        print "A New Mystery..."
         for cur_byte in xrange(block_size-1, -1, -1):
             peek = offering("A" * cur_byte + cur_mystery, key)[:block_size]
             for cur_char in xrange(256):
@@ -56,11 +56,8 @@ def ecb_oracle_attack():
                 if possible == peek:
                     cur_answer = cur_answer + chr(cur_char)
                     answer = answer + chr(cur_char)
-                    print "FOUND:", answer
                     break
-                if cur_char == 255:
-                    print "ERROR, unable to match byte#:", cur_byte
-                    return
-        print "Message Decoded Sucessfully:", answer
+    print "Message Decoded Sucessfully:", answer
 
-ecb_oracle_attack()
+ecb_oracle_attack("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK".decode("base-64"))
+    
